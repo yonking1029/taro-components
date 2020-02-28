236430bd36372802a652d8e408b676553e883c39
158249 <wangyangqing@ky-tech.com.cn>
2020-02-28 19:59:41 +0800

    有改了一点组件

--- src/components/retailers/new-products/index.tsx
+++ src/components/retailers/new-products/index.tsx
@@ -1,5 +1,5 @@
 import Taro, { Component, Config } from "@tarojs/taro"
-import { View, Image } from "@tarojs/components"
+import { View, Image, ScrollView } from "@tarojs/components"
 import PropTypes from 'prop-types'
 
 /**
@@ -26,23 +26,26 @@ export default class Index extends Component {
     return (
       <View>
         <View>上面一个图片表示类目</View>
-        {
-          list.map(item => {
-            return (<View>
-              <View>
-                <Image className='w_200 h_200' src={item.url}></Image>
-              </View>
-              <View className=''>{item.label}</View>
-              <View className=''>
-                <View>{item.price}</View>
-                <View>
-                  <View>+</View>
-                </View>
-              </View>
-            </View>)
-          })
-        }
-      新品（上面一个图片表示类目，下面8个商品滚动+查看更多）（食品，居家用品，百货，母婴）
+        <ScrollView scrollX className='w_670 of_h'>
+          <View className='flex'>
+            {
+              list.map(item => {
+                return (<View className='w_210 p_10'>
+                  <View>
+                    <Image className='w_170 h_180' src={item.url}></Image>
+                  </View>
+                  <View className='f_s_22 c_999'>{item.label}</View>
+                  <View className='flex flex_jc_b flex_ai_c'>
+                    <View className='c_red f_s_24'>¥{item.price}</View>
+                    <View>
+                      <View className='flex flex_jc_c flex_ai_c c_red f_s_24 b_solid_e70013 b_radius_50 w_30 h_30'>+</View>
+                    </View>
+                  </View>
+                </View>)
+              })
+            }
+          </View>
+        </ScrollView>
       </View>
     );
   }
